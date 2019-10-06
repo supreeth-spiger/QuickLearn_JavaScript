b6bf03bd991c393ff77a78e4c97f5c36796cb57f
Supreeth R <supreethr726@gmail.com>
2019-10-06 14:50:49 +0530

    init update

--- PromiseAll_AsyncAwait/script.js
+++ PromiseAll_AsyncAwait/script.js
@@ -1,25 +1,26 @@
-
-const api = 'https://jsonplaceholder.typicode.com/users';
+const api = "https://jsonplaceholder.typicode.com/users";
 
 const starter = () => {
   let promises;
-  for(let i = 1; i<= 10; i++){
-  promises = [fetchApi(i)]
-  
-  Promise.all(promises)
-  .then(result => {
-    for (let j= 0; j< result.length; j++){
-      console.log(`\n UserID: ${result[j].id}\n NAME: ${result[j].name}\n Email: ${result[j].email}\n`)
-    }
-  })
-  .catch(err => console.log(err));
+  for (let i = 1; i <= 10; i++) {
+    promises = [fetchApi(i)];
+
+    Promise.all(promises)
+      .then(result => {
+        for (let j = 0; j < result.length; j++) {
+          console.log(
+            `\n UserID: ${result[j].id}\n NAME: ${result[j].name}\n Email: ${result[j].email}\n`
+          );
+        }
+      })
+      .catch(err => console.log(err));
   }
 };
 
-const fetchApi = async (num) => {
+const fetchApi = async num => {
   let response = await fetch(`${api}/${num}`);
   let data = await response.json();
-  return data
+  return data;
 };
 
 starter();
